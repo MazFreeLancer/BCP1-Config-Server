002e29779ff3e00f853816cc252acabc82506927
MZena <manuelzenareque@gmail.com>
2022-03-01 14:48:03 -0500

    Config server app

---
+++ src/main/java/com/nttdata/bcp1/configserver/ConfigServer/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bcp1.configserver.ConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
